7e01d72637d30b9e79e6840e3d96f049f57d18c0
BOUANANI YASSER <bouyasser77@gmail.com>
2022-11-04 16:54:17 +0100

    added spin animation to toggle theme button

--- components/UI/Toggler.tsx
+++ components/UI/Toggler.tsx
@@ -6,18 +6,30 @@ import { FaSun } from 'react-icons/fa';
 const Toggler = () => {
    const { theme, setTheme } = useTheme();
    const [mounted, setMounted] = useState(false);
+   const [clicked, setClicked] = useState(false);
    useEffect(() => setMounted(true), []);
+
+   const clickHandler = () => {
+      setClicked(true);
+      setTimeout(() => {
+         setClicked(false);
+      }, 1000);
+      setTheme(theme === 'light' ? 'dark' : 'light');
+   };
+
    if (!mounted) return null;
    return (
       <button
-         className="w-8 h-8 bg-blue-100 rounded-full flex items-center justify-center dark:bg-slate-900 hover:ring-2 ring-blue-400 transition-all duration-300 focus:outline-none"
-         onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+         className={`${
+            clicked ? 'spin' : ''
+         } w-8 h-8 bg-blue-100 rounded-full flex items-center justify-center  hover:ring-2 ring-blue-400 transition-all duration-300 focus:outline-none`}
+         onClick={clickHandler}
          aria-label="Toggle Dark Mode"
       >
          {theme === 'light' ? (
             <GiMoon className="text-[#36A9DE] w-5 h-5" />
          ) : (
-            <FaSun className="text-white w-5 h-5" />
+            <FaSun className="text-yellow-500 w-5 h-5" />
          )}
       </button>
    );
