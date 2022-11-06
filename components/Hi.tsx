533e2280d904ea93e7904cfff0455c32c80c343c
BOUANANI YASSER <bouyasser77@gmail.com>
2022-11-06 21:07:08 +0100

    redesign hero section

--- components/Hi.tsx
+++ components/Hi.tsx
@@ -1,12 +1,6 @@
 import { FC } from 'react';
 import { motion } from 'framer-motion';
 
-import { BsTwitter } from 'react-icons/bs';
-import { TbBrandLinkedin } from 'react-icons/tb';
-import { SiFreelancer } from 'react-icons/si';
-import { VscGithub } from 'react-icons/vsc';
-import { ImWhatsapp } from 'react-icons/im';
-
 const Hi: FC = () => {
    const container = {
       hidden: { opacity: 0, x: -40 },
@@ -30,23 +24,23 @@ const Hi: FC = () => {
    };
 
    return (
-      <div className="relative pt-10 bg-gradient-to-br from-base-300 to-secondary text-primary-content h-[100vh]">
+      <div className="relative pt-10 bg-gradient-to-br from-base-200 dark:from-slate-900 to-secondary text-primary-content h-[100vh]">
          <motion.div
-            className="mx-[10%] py-[3%]  max-w-[80%]"
+            className="mx-[5%] py-[3%]  max-w-[80%]"
             variants={container}
             initial="hidden"
             animate="show"
          >
             <div className="flex-1 px-24 py-2">
-               <div className="text-4xl text-left text-secondary  leading-relaxed font-bold ...">
-                  <motion.h1 variants={item} className="text-5xl ">
+               <div className="text-xl text-primary-content text-left   leading-relaxed font-bold ...">
+                  <motion.h1 variants={item} className=" ">
                      Hi, I am
                   </motion.h1>
                   <motion.div
                      variants={item}
                      className="bg-1 title-gradient  "
                      style={{
-                        fontSize: 'clamp(40px, 8vw, 80px)',
+                        fontSize: 'clamp(30px, 8vw, 50px)',
                         background: 'var(primary)',
                      }}
                   >
@@ -55,10 +49,86 @@ const Hi: FC = () => {
                   <motion.div
                      variants={item}
                      className=" bg-2 title-gradient"
-                     style={{ fontSize: 'clamp(20px, 8vw, 50px)' }}
+                     style={{ fontSize: 'clamp(20px, 8vw, 30px)' }}
                   >
-                     Software Engineer & Fullstack Developer
+                     A Software Engineer & Fullstack Developer
                   </motion.div>
+                  <div className="flex w-full flex-col items-center space-y-10 lg:flex-row lg:items-start lg:space-x-4 lg:space-y-0">
+                     <div className="my-2 flex max-w-sm flex-col gap-2 text-left">
+                        <div className="flex items-center g gap-2">
+                           <svg
+                              width="20"
+                              height="20"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              className="inline-block h-6 w-6 stroke-current"
+                           >
+                              <path
+                                 stroke-linecap="round"
+                                 stroke-linejoin="round"
+                                 stroke-width="2"
+                                 d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                              ></path>
+                           </svg>
+                           Web & Mobile Development
+                        </div>
+                        <div className="flex items-center gap-2">
+                           <svg
+                              width="20"
+                              height="20"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              className="inline-block h-6 w-6 stroke-current"
+                           >
+                              <path
+                                 stroke-linecap="round"
+                                 stroke-linejoin="round"
+                                 stroke-width="2"
+                                 d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                              ></path>
+                           </svg>
+                           Product design (UX/UI)
+                        </div>
+                        <div className="flex items-center g gap-2">
+                           <svg
+                              width="20"
+                              height="20"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              className="inline-block h-6 w-6 stroke-current"
+                           >
+                              <path
+                                 stroke-linecap="round"
+                                 stroke-linejoin="round"
+                                 stroke-width="2"
+                                 d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                              ></path>
+                           </svg>
+                           Logo design
+                        </div>
+                        <div className="flex items-center  gap-2">
+                           <svg
+                              width="20"
+                              height="20"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              className="inline-block h-6 w-6 stroke-current"
+                           >
+                              <path
+                                 stroke-linecap="round"
+                                 stroke-linejoin="round"
+                                 stroke-width="2"
+                                 d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                              ></path>
+                           </svg>
+                           project management
+                        </div>
+                     </div>
+                  </div>
                </div>
             </div>
             <div className="mt-4 flex flex-1 justify-center space-x-2 lg:mt-6 lg:ml-24 lg:justify-start">
@@ -70,14 +140,14 @@ const Hi: FC = () => {
                </motion.button>
                <motion.button
                   variants={actionsItem}
-                  className="z-10 btn glass text-primary lg:btn-lg normal-case"
+                  className="z-10 btn btn-primary btn-outline text-base-200 lg:btn-lg normal-case"
                >
                   Works
                </motion.button>
             </div>
          </motion.div>
          <svg
-            className=" absolute -bottom-20 z-1 fill-secondary col-start-1 row-start-1 h-auto w-full"
+            className=" dark:fill-slate-700 absolute -bottom-20 z-1 fill-secondary col-start-1 row-start-1 h-auto w-full"
             width="1600"
             height="595"
             viewBox="0 0 1600 595"
@@ -86,25 +156,6 @@ const Hi: FC = () => {
          >
             <path d="M0 338L53.3 349.2C106.7 360.3 213.3 382.7 320 393.8C426.7 405 533.3 405 640 359.3C746.7 313.7 853.3 222.3 960 189.2C1066.7 156 1173.3 181 1280 159.2C1386.7 137.3 1493.3 68.7 1546.7 34.3L1600 0V595H1546.7C1493.3 595 1386.7 595 1280 595C1173.3 595 1066.7 595 960 595C853.3 595 746.7 595 640 595C533.3 595 426.7 595 320 595C213.3 595 106.7 595 53.3 595H0V338Z"></path>
          </svg>
-         <div className="absolute top-20 left-[5%]">
-            <ul className="flex flex-col  justify-center items-center">
-               <li>
-                  <BsTwitter className="my-icon" />
-               </li>
-               <li>
-                  <TbBrandLinkedin className="my-icon" />
-               </li>
-               <li>
-                  <SiFreelancer className="my-icon fr" />
-               </li>
-               <li>
-                  <VscGithub className="my-icon" />
-               </li>
-               <li>
-                  <ImWhatsapp className="my-icon" />
-               </li>
-            </ul>
-         </div>
       </div>
    );
 };
--- components/UI/nav.tsx
+++ components/UI/nav.tsx
@@ -1,20 +1,39 @@
 import Toggler from './Toggler';
 import { useTheme } from 'next-themes';
 import { FaPaintBrush } from 'react-icons/fa';
+import { TbBrandLinkedin } from 'react-icons/tb';
+import { SiFreelancer } from 'react-icons/si';
+import { VscGithub } from 'react-icons/vsc';
+import { ImWhatsapp } from 'react-icons/im';
 
 function Nav() {
    const { theme, setTheme } = useTheme();
    return (
       <header className="absolute top-0 z-10 w-full">
-         <nav className="flex justify-center items-center align-middle max-w-[90%] h-[70px] m-auto ">
-            <div className="text-neutral w-1/3 p-auto my-auto  font-bold">{`<YasserBouanani/>`}</div>
+         <nav className="flex justify-between items-center align-middle max-w-[80%] h-[70px] m-auto ">
+            <div className="">
+               <ul className="flex justify-center items-center">
+                  <li>
+                     <ImWhatsapp className="my-icon" />
+                  </li>
+                  <li>
+                     <VscGithub className="my-icon" />
+                  </li>
+
+                  <li>
+                     <TbBrandLinkedin className="my-icon" />
+                  </li>
+                  <li>
+                     <SiFreelancer className="my-icon fr" />
+                  </li>
+               </ul>
+            </div>
+            {/* <div className="text-neutral w-1/3 p-auto my-auto  font-bold">{`<YasserBouanani/>`}</div> */}
             <ul className="flex flex-row  space-x-10 justify-center items-center d w-1/2 font-bold">
                <li className="p-1 m-1 cursor-pointer">Hi</li>
                <li className="p-1 m-1 cursor-pointer">Works</li>
                <li className="p-1 m-1 cursor-pointer">Connect</li>
-               <button className="w-24 h-8 text-black bg-blue-100 rounded-lg  flex items-center justify-center hover:ring-2 ring-primary transition-all duration-300 focus:outline-none">
-                  Resume
-               </button>
+               <button className="w-24 h-8 btn btn-sm glass">Resume</button>
                <div className="dropdown dropdown-end flex justify-center items-center">
                   <label
                      tabIndex={0}
