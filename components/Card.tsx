b6c053d12455ec8c51a4850394e5ad4d652ac3c2
BOUANANI YASSER <bouyasser77@gmail.com>
2022-11-08 08:53:17 +0100

    made my works section mobile v

--- components/Card.tsx
+++ components/Card.tsx
@@ -11,11 +11,11 @@ const Card: FC<CardProps> = ({ path }) => {
             className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
             style={{ backgroundImage: `url(${path})` }}
          >
-            <div className="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
-               <h2 className="mt-4 text-2xl font-semibold text-white capitalize">
+            <div className="flex flex-col justify-center w-full h-full px-4 py-2 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+               <h2 className="mt-1 text-2xl font-semibold text-white capitalize">
                   Website Name
                </h2>
-               <p className="mt-2 text-sm tracking-wider text-blue-400 uppercase ">
+               <p className="mt-1 text-sm tracking-wider text-blue-400 uppercase ">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                   Quaerat, sit!
                </p>
--- components/GridCards.tsx
+++ components/GridCards.tsx
@@ -7,11 +7,11 @@ const GridCards: FC<GridCardsProps> = () => {
    const works = ['maloma.png.3', 'n.jpg.5', 'wms.jpg.5', 'n2.png.2'];
    //    const gallery = useRef<HTMLDivElement>(null);
    return (
-      <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
+      <div className="grid justify-center items-center m-20 grid-cols-1 gap-6 mt-6 xl:mt-8 xl:gap-8 md:grid-cols-2 xl:grid-cols-3">
          {works.map((el, index) => (
             <div
                key={index}
-               className="relative max-h-[300px] min-w-[300px] overflow-hidden rounded-lg shadow-2xl dark:shadow-gray-600"
+               className="relative max-h-[200px] min-w-[200px] w-[95%] md:w-[80%] overflow-hidden rounded-lg shadow-2xl dark:shadow-gray-600"
             >
                <Gallery folder={el} />
             </div>
